4b2d8e03082a02eef3919cff488e431f41dcd174
Matthew Galajda <mgalajda@nd.edu>
2021-08-18 15:10:49 -0400

    Fun

--- Fun.R
+++ Fun.R
@@ -1,1 +1,8 @@
-y <- 1:11
+library(ggplot2)
+my_plot <- ggplot(data = mtcars,
+      mapping = aes(x = disp, y =mpg))+
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_minimal()
+
+plotly::ggplotly(my_plot)
